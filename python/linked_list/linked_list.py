0978a3e2994d6d6efb463ae8ff0dac02fe3ddd85
Zach Hornung <Zachhornung@comcast.net>
2021-05-22 00:40:20 -0700

    basic functionality is working

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,39 @@
 class LinkedList:
-    """
-    Put docstring here
-    """
-
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, value):
+        self.head = Node(value, self.head)
+
+    # def traverse(self, func):
+    #     current = self.head
+    #     while current:
+    #         func()
+    #     current = current.next
+
+    def includes(self, search_value):
+        # look_for_value = lambda value: True if value is search_value
+        # self.traverse(look_for_value(search_value))
+        # return False
+        current = self.head
+        while current:
+            if current.value == search_value:
+                return True
+            current = current.next
+        return False
+
+    def __str__(self):
+        string = ''
+        current = self.head
+        while current:
+            string += f'{current.value} --> '
+            current = current.next
+        string += 'None'
+        return string
+
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,37 @@
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import LinkedList, Node
+import pytest
 
 
 def test_import():
     assert LinkedList
+
+
+def test_linked_list_exits():
+    lst = LinkedList()
+    assert lst
+
+def test_insert_node():
+    lst = LinkedList()
+    lst.insert('baby goats')
+    assert lst.head.value == 'baby goats'
+
+def test_insert_multiple_things():
+    lst = LinkedList()
+    lst.insert('baby goats')
+    lst.insert('more goats')
+    assert lst.head.next.value == 'baby goats'
+
+def test_includes_method():
+    lst = LinkedList()
+    lst.insert('baby goats')
+    lst.insert('more goats')
+    assert lst.includes('baby goats')
+
+def test_to_string_method():
+    lst = LinkedList()
+    lst.insert('baby goats')
+    lst.insert('more goats')
+    assert str(lst) == 'more goats --> baby goats --> None'
+
+
+
