d4b9e27204e984c963c99b213a332769ed8baf24
Zach Hornung <Zachhornung@comcast.net>
2021-05-22 02:04:31 -0700

    tests pass

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -5,16 +5,7 @@ class LinkedList:
     def insert(self, value):
         self.head = Node(value, self.head)
 
-    # def traverse(self, func):
-    #     current = self.head
-    #     while current:
-    #         func()
-    #     current = current.next
-
     def includes(self, search_value):
-        # look_for_value = lambda value: True if value is search_value
-        # self.traverse(look_for_value(search_value))
-        # return False
         current = self.head
         while current:
             if current.value == search_value:
@@ -30,8 +21,6 @@ class LinkedList:
             current = current.next
         string += 'None'
         return string
-
-
 
 class Node:
     def __init__(self, value, next=None):
