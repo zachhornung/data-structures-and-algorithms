83412de1db341dee5f624a3d12270a063d4a5fb5
Zach Hornung <Zachhornung@comcast.net>
2021-05-24 14:36:21 -0700

    added functions

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,3 +1,32 @@
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
+
+#     def insert(self, value):
+#         self.head = Node(value, self.head)
+
+#     def includes(self, search_value):
+#         current = self.head
+#         while current:
+#             if current.value == search_value:
+#                 return True
+#             current = current.next
+#         return False
+
+#     def __str__(self):
+#         string = ''
+#         current = self.head
+#         while current:
+#             string += f'{current.value} --> '
+#             current = current.next
+#         string += 'None'
+#         return string
+
+# class Node:
+#     def __init__(self, value, next=None):
+#         self.value = value
+#         self.next = next
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -21,6 +50,43 @@ class LinkedList:
             current = current.next
         string += 'None'
         return string
+
+
+    def append(self, value):
+        current = self.head
+        while current:
+            if current.next == None:
+                current.next = Node(value)
+                break
+            current = current.next
+
+
+    def insert_before(self, value, new_value):
+        current = self.head
+        previous = None
+        while current:
+            if self.head.value == value:
+                self.insert(new_value)
+                break
+            if current.value == value:
+                previous.next = Node(new_value, current)
+                break
+            previous = current
+            current = current.next
+
+    def insert_after(self, value, new_value):
+        current = self.head
+        insert_before_this = current.next
+        while current:
+            if self.head.value == value:
+                self.insert_before(insert_before_this.value, new_value)
+                break
+            if current.value == value:
+                current.next = Node(new_value, insert_before_this)
+                break
+            current = current.next
+            insert_before_this = current.next
+
 
 class Node:
     def __init__(self, value, next=None):
