871175de60050e3109102e6a663ee96c14270a8d
Zach Hornung <Zachhornung@comcast.net>
2021-05-24 14:48:23 -0700

    added tests

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,32 +1,3 @@
-# class LinkedList:
-#     def __init__(self):
-#         self.head = None
-
-#     def insert(self, value):
-#         self.head = Node(value, self.head)
-
-#     def includes(self, search_value):
-#         current = self.head
-#         while current:
-#             if current.value == search_value:
-#                 return True
-#             current = current.next
-#         return False
-
-#     def __str__(self):
-#         string = ''
-#         current = self.head
-#         while current:
-#             string += f'{current.value} --> '
-#             current = current.next
-#         string += 'None'
-#         return string
-
-# class Node:
-#     def __init__(self, value, next=None):
-#         self.value = value
-#         self.next = next
-
 class LinkedList:
     def __init__(self):
         self.head = None
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -33,5 +33,50 @@ def test_to_string_method():
     lst.insert('more goats')
     assert str(lst) == 'more goats --> baby goats --> None'
 
+def test_append():
+    lst = LinkedList()
+    lst.insert('baby goats')
+    lst.insert('more goats')
+    lst.append('water')
+    assert str(lst) == 'more goats --> baby goats --> water --> None'
+
+def test_insert_before():
+    lst = LinkedList()
+    lst.insert('baby goats')
+    lst.insert('more goats')
+    lst.append('water')
+    lst.insert_before('baby goats', 'before')
+    assert str(lst) == 'more goats --> before --> baby goats --> water --> None'
+
+def test_insert_after():
+    lst = LinkedList()
+    lst.insert('baby goats')
+    lst.insert('more goats')
+    lst.append('water')
+    lst.insert_before('baby goats', 'before')
+    lst.insert_after('baby goats', 'after')
+    assert str(lst) == 'more goats --> before --> baby goats --> after --> water --> None'
+
+def test_insert_before_first_node():
+    lst = LinkedList()
+    lst.insert('baby goats')
+    lst.insert('more goats')
+    lst.append('water')
+    lst.insert_before('baby goats', 'before')
+    lst.insert_after('baby goats', 'after')
+    lst.insert_before('more goats', 'it worked!')
+    assert str(lst) == 'it worked! --> more goats --> before --> baby goats --> after --> water --> None'
+
+def test_insert_after_last_node():
+    lst = LinkedList()
+    lst.insert('baby goats')
+    lst.insert('more goats')
+    lst.append('water')
+    lst.insert_before('baby goats', 'before')
+    lst.insert_after('baby goats', 'after')
+    lst.insert_before('more goats', 'it worked!')
+    lst.insert_after('water', 'drought')
+    assert str(lst) == 'it worked! --> more goats --> before --> baby goats --> after --> water --> drought --> None'
+
 
 
