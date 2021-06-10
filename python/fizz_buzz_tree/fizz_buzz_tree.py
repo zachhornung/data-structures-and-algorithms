39d94df875976d045d5d57e068684f2f66c1a2d4
Zach Hornung <Zachhornung@comcast.net>
2021-06-10 10:56:05 -0700

    getting closer

--- python/fizz_buzz_tree/fizz_buzz_tree.py
+++ python/fizz_buzz_tree/fizz_buzz_tree.py
@@ -1,20 +1,35 @@
 from stacks_and_queues.stacks_and_queues import Queue, Node
 
 class KNode:
-  def __init__(self, value=None, children={}):
+  def __init__(self, value=None, children={}, previous_value=None):
+    self.previous_value = previous_value
     self.value = value
     self.children = children
 
 class KAry_tree:
-  def __init__(self, root=None):
+  def __init__(self, k=5, root=None):
+    self.k = k
     self.root = root
+    self.map = {}
     
-  def add(self, value, node=None):
-    node_to_add = KNode(value)
     
-    if node:
-      node_to_add == node
-      value = node.value
+        
+  # def walk(node, node_to_add):
+  #   if not node.children:
+  #     return
+  #   for index in node.children:
+  #     if node.children[index] == None
+  #   child_number = len(node.children)
+  #   walk(node.children[child_number+1], node_to_add)
+      
+    
+  def add(self, **kwargs):
+    if 'value' in kwargs:
+      node_to_add = KNode(kwargs['value'])
+      value = kwargs['value']
+    if 'node' in kwargs and not 'value' in kwargs:
+      node_to_add = kwargs['node']
+      value = node_to_add.value
       
     if not self.root:
       self.root = node_to_add
@@ -25,50 +40,88 @@ class KAry_tree:
       return
     
     current = self.root
-    running = True
-    while running:
+    while True:
+      # if this is where the node is supossed to be, then put it here and break the loop.
       if (value in current.children) and (current.children[value] == None):
         current.children[value] == node_to_add
-        running = False
-        continue
+        break
       #check to see if the value points to a node that is supossed to have that value as a child. if it does, go to that node.
       if (value in current.children) and (current.children[value] != None):
+        # i think i need recursive walk call here to check out all of the children in the current and next nodes to see if the 
+        # current node is suppossed to go further down the tree
         current = current.children[value]
         value = current.children[value].value
-      else:
-        
+        continue
       # if it doesnt, then set a child node for the current value to be 
-        print('im looping')
-        current.children[value] = node_to_add
-        running = False
-    
-      
+      print('looping')
+      current.children[value] = node_to_add
+      break
+  
+  def copy(self):
+    if not self.root:
+      return
+      # make a copy of the tree and just put the root and its children in there as a new node
+    clone_root = KNode(self.root.value)
+    copy_tree = KAry_tree()
+    copy_tree.root = clone_root
 
-def fizz_buzz_tree(tree=None):
-  if not tree.root:
+    queue = Queue()
+    queue.enqueue((self.root, clone_root))
+
+    while queue.peek():
+      og_node, copy_node = queue.dequeue()
+    
+      if og_node.children:
+        for child in og_node.children:
+          copy_child = KNode(og_node.children[child].value)
+          queue.enqueue((og_node.children[child], copy_child))
+          copy_node.children[child] = copy_child
+    
+    return copy_tree
+
+def fizz_buzz_tree(tree_og=None):
+  if not tree_og or not tree_og.root:
     return
+  # make a copy of the tree and just put the root and its children in there as a new node
+  copy_tree = KAry_tree()
+  copy_tree.root = KNode(tree_og.root.value)
   
   queue = Queue()
-  new_fizzbuzz_tree = KAry_tree()
-  queue.enqueue(tree.root)
+  queue.enqueue((tree_og.root, copy_tree.root))
   
   while queue.peek():
-    current = queue.dequeue()
-    if current.children:
-      for child in current.children:
-        queue.enqueue(current.children[child])
-      
-    old_value = current.value
+    og_node, copy_node = queue.dequeue()
     
-    if int(current.value) % 15 == 0:
-      current.value == 'fizzbuzz'
-    elif int(current.value) % 5 == 0:
-        current.value = 'buzz'
-    elif int(current.value) % 3 == 0:
+    if og_node.children:
+      for child in og_node.children:
+        copy_child = KNode(og_node.children[child].value)
+        queue.enqueue((og_node.children[child], copy_child))
+        copy_node.children[child] = copy_child
+        
+    
+    # keys = og_node.children.keys()
+    # for key in keys:
+    #   if int(key) % 3 == 0:
+    #     copy_node.children['fizz'] = current.children.pop(key)
+    #   if int(key) % 5 == 0:
+    #     current.children['buzz'] = current.children.pop(key)
+    #   if int(key) % 15 == 0:
+    #     current.children['fizzbuzz'] = current.children.pop(key)
+    # i think i also need to change the key values for a child if the key value will be changed to fizz, buzz or fizzbuzz
+    # so that when i add it to the new kary tree it will know where it goes and where its kids go
+    
+    
+    
+    if current.value % 3 == 0:
       current.value = 'fizz'
+    
+    if current.value % 5 == 0:
+        current.value = 'buzz'
+      
+    if current.value % 15 == 0:
+      current.value == 'fizzbuzz'
 
-    current.value = str(current.value)
-    new_fizzbuzz_tree.add(old_value, current)
+    new_fizzbuzz_tree.add(node=current)
     
   return new_fizzbuzz_tree
     
--- python/tests/test_fizz_buzz_tree.py
+++ python/tests/test_fizz_buzz_tree.py
@@ -1,24 +1,40 @@
 from fizz_buzz_tree.fizz_buzz_tree import fizz_buzz_tree, KAry_tree, KNode
 
-# def test_import():
-#   assert fizz_buzz_tree
+def test_import():
+  assert fizz_buzz_tree
 
-# def test_KAry_tree_and_KNode_import():
-#   assert KAry_tree and KNode
+def test_KAry_tree_and_KNode_import():
+  assert KAry_tree and KNode
 
-# def test_KAry_tree_add_initial_value():
-#   k_tree = KAry_tree()
-#   k_tree.add('babies')
-#   assert k_tree.root.value == 'babies' 
+def test_KAry_tree_add_initial_value():
+  k_tree = KAry_tree(4)
+  k_tree.add(node=KNode('babies', {'chickens': None}))
+  assert k_tree.root.value == 'babies' and k_tree.root.children['chickens'] == None
   
-# def test_fizz_buzz_tree():
-#   k_tree = KAry_tree()
+def test_fizz_buzz_tree():
+  k_tree = KAry_tree(4)
+  five = KNode(5)
+  six = KNode(6)
+  fifteen = KNode(15)
+  eight = KNode(8)
+  three = KNode(3)
+  
+  five.children[6] = six
+  five.children[8] = eight
+  five.children[3] = three
+  six.children[15] = fifteen
+  k_tree.add(node=five)
+  clone_tree = k_tree.copy()
+  assert k_tree.root.children[6].children[15].value == k_tree.root.children[6].children[15].value
+  
+# def test_buzzy_children():
+#   k_tree = KAry_tree(3)
 #   node = KNode('5', {'6': None})
-#   k_tree.add('5', node)
+#   k_tree.add(node=node)
 #   another_node = KNode('6', {'15': None})
-#   k_tree.add('6', another_node)
-#   k_tree.add('15')
-#   k_tree.add('8')
-#   k_tree.add('3')
+#   k_tree.add(node=another_node)
+#   k_tree.add(value='15')
+#   k_tree.add(value='8')
+#   k_tree.add(value='3')
 #   fizzbuzz_tree = fizz_buzz_tree(k_tree)
-#   assert fizzbuzz_tree.root.value == 'buzz'
+#   assert 'fizz' == fizzbuzz_tree.root.children['6'].value
