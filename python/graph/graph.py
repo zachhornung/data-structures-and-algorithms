d14952739a7bade99978c105484bbc7a779f7ae2
Zach Hornung <Zachhornung@comcast.net>
2021-07-06 15:49:32 -0700

    tests pass on the business_trip function

--- python/graph/graph.py
+++ python/graph/graph.py
@@ -1,53 +1,72 @@
 from stacks_and_queues.stacks_and_queues import Node ,Queue
 from collections import deque
 
-class Vertex:
-  def __init__(self, value):
-    self.value = value
-    
-  
 class Graph:
-  def __init__(self) -> None:
-      self.adjacency_list = {}
+    def __init__(self,kind='single direction'):
+        self._adjacency_list = {}
+        self.type = kind
+
+
+    def add_vertex(self, value):
+        v = Vertex(value)
+        if v not in self._adjacency_list:
+          self._adjacency_list[v] = {}
+          return v
+
+    def size(self):
+        return len(self._adjacency_list)
+
+    def add_edge(self, start_vertex, end_vertex, weight=0):
+        if start_vertex not in self._adjacency_list:
+            raise KeyError("Start Vertex not found in graph")
+
+        if end_vertex not in self._adjacency_list:
+            raise KeyError("End Vertex not found in graph")
+
+        if start_vertex not in self._adjacency_list:
+          self.add_node(start_vertex)
+
+        self._adjacency_list[start_vertex][end_vertex] = weight
+
+        if self.type == 'bidirectional':
+          self._adjacency_list[end_vertex][start_vertex] = weight
+
+
+    def get_nodes(self):
+        return self._adjacency_list.keys()
+
+    def get_neighbors(self, vertex):
+        return self._adjacency_list.get(vertex)
       
-    
-  def add_vertex(self,value):
-    new_vertex = Vertex(value)
-    self.adjacency_list[new_vertex] = {}
-    return new_vertex
-  
-  def add_edge(self, start_node, end_node, weight=0):
-    self.adjacency_list[start_node][end_node] = weight
-  
-  def get_vertexes(self):
-    return self.adjacency_list
-  
-  def get_neighbors(self, vertex):
-    return self.adjacency_list[vertex]
-  
-  def size(self):
-    return len(self.adjacency_list)
-  
-  def traverse(self, node, debug=False):
-    queue = Queue()
-    ret_baby = []
-    if not node:
-      return 'babies'
-    visited = {}
-    queue.enqueue(node)
-    while queue.peek():
-      current = queue.dequeue()
-      visited[current] = True
-      if current == 'this queue is empty buddy':
-        break
-      for baby_node in self.adjacency_list[current]:
-        if baby_node == None:
+    def traverse(self, node, debug=False):
+      queue = Queue()
+      ret_baby = []
+      if not node:
+        return 'babies'
+      visited = {}
+      queue.enqueue(node)
+      while queue.peek():
+        current = queue.dequeue()
+        visited[current] = True
+        if current == 'this queue is empty buddy':
           break
-        if not baby_node in visited:
-          queue.enqueue(baby_node)
-      ret_baby.append(current.value)
-      if debug:
-        breakpoint()
-    return ret_baby
+        for baby_node in self._adjacency_list[current]:
+          if baby_node == None:
+            break
+          if not baby_node in visited:
+            queue.enqueue(baby_node)
+        ret_baby.append(current.value)
+        if debug:
+          breakpoint()
+      return ret_baby
+    
+    
+
+
+class Vertex:
+    def __init__(self, value):
+        self.value = value
+  
+
       
     
--- python/tests/test_graph.py
+++ python/tests/test_graph.py
@@ -29,8 +29,8 @@ def test_can_add_edge():
   babies = graph.add_vertex('babies')
   goats = graph.add_vertex('goats')
   graph.add_edge(babies, goats, 10)
-  assert goats in graph.adjacency_list[babies]
-  assert graph.adjacency_list[babies][goats] == 10
+  assert goats in graph._adjacency_list[babies]
+  assert graph._adjacency_list[babies][goats] == 10
   
 def test_get_neighbors():
   graph = Graph()
@@ -49,9 +49,7 @@ def test_insert_lots_of_things_added():
     for animal1 in barnyard:
       for animal2 in barnyard:
         if animal1.value in animal2.value[1:]:
-          n1, n2 = animal1.value, animal2.value
-          assert graph.adjacency_list[animal1][animal2] == 'double food for you guys'
-          print(n1,n2)
+          assert graph._adjacency_list[animal1][animal2] == 'double food for you guys'
           if debug:
             breakpoint()
             
@@ -66,28 +64,65 @@ def test_breadth_first_traversal():
   monstroplolis = graph.add_vertex('Monstroplolis')
   narnia = graph.add_vertex('Narnia')
   naboo = graph.add_vertex('Naboo')
+  
   graph.add_edge(pandora, arendelle)
-  # graph.add_edge(arendelle, pandora)
   graph.add_edge(arendelle, metroville)
-  # graph.add_edge(metroville, arendelle)
   graph.add_edge(arendelle, monstroplolis)
-  # graph.add_edge(monstroplolis, arendelle)
-  # graph.add_edge(metroville, monstroplolis)
   graph.add_edge(monstroplolis, metroville)
   graph.add_edge(metroville, narnia)
-  # graph.add_edge(narnia, metroville)
   graph.add_edge(metroville, naboo)
-  # graph.add_edge(naboo,metroville)
   graph.add_edge(monstroplolis, naboo)
-  # graph.add_edge(naboo, monstroplolis)
   graph.add_edge(naboo, narnia)
-  # graph.add_edge(narnia, naboo)
 
   babies = graph.traverse(pandora)
-  # breakpoint()
+  
   assert babies == ['Pandora', 'Arendelle', 'Metroville', 'Monstroplolis', 'Naboo', 'Narnia']
   
 
+def test_city_path_finder():
+  def business_trip(graph, arr_cities):
+    if not graph._adjacency_list:
+      return False, 0
+
+    cost = 0
+    for i,city in enumerate(arr_cities):
+      neighbors = graph.get_neighbors(city)
+      if i+1 >=len(arr_cities): # if we're at the end of the list
+        return True, cost
+      if arr_cities[i+1] in neighbors:
+        cost += neighbors.get(arr_cities[i+1])
+        continue
+      return False, 0
+  
+  graph = Graph('bidirectional')
+  
+  pandora = graph.add_vertex('Pandora')
+  arendelle = graph.add_vertex('Arendelle')
+  metroville = graph.add_vertex('Metroville')
+  monstroplolis = graph.add_vertex('Monstroplolis')
+  narnia = graph.add_vertex('Narnia')
+  naboo = graph.add_vertex('Naboo')
+
+  cities1 = [metroville, pandora]
+  cities2 = [arendelle, monstroplolis, naboo]
+  cities3 = [naboo, pandora]
+  cities4 = [narnia, arendelle, naboo]
+  test_cases = [cities1, cities2, cities3, cities4]
+  expected_results = [(True, 82), (True, 115), (False, 0), (False, 0)]
+
+  graph.add_edge(pandora, arendelle, 150)
+  graph.add_edge(pandora, metroville, 82)
+  graph.add_edge(arendelle, metroville, 99)
+  graph.add_edge(arendelle, monstroplolis, 42)
+  graph.add_edge(metroville, monstroplolis, 105)
+  graph.add_edge(metroville, narnia, 37)
+  graph.add_edge(metroville, naboo, 26)
+  graph.add_edge(naboo, narnia, 250)
+  graph.add_edge(monstroplolis, naboo, 73)
+  
+  for i,test in enumerate(test_cases):
+    assert business_trip(graph, test) == expected_results[i]
+  
   
   
   
